9027351b962a972c2156b40cbc77edb63fd1437d
axel <ssaxel03@gmail.com>
2024-11-20 23:04:42 +0000

    feat: added base parallax

--- scripts/player.gd
+++ scripts/player.gd
@@ -15,13 +15,13 @@ func _physics_process(delta: float) -> void:
 		velocity += 7 * get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		animated_sprite.play("jump")
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("move_left", "move_right")
 	
 	if direction:
 		velocity.x = direction * SPEED
@@ -30,13 +30,17 @@ func _physics_process(delta: float) -> void:
 		
 	if is_on_floor():
 		if direction > 0:
-			animated_sprite.flip_h = false
 			animated_sprite.play("run")
 		elif direction < 0:
-			animated_sprite.flip_h = true
 			animated_sprite.play("run")
 		else:
 			animated_sprite.play("idle")
+	else:
+		animated_sprite.play("jumping")
 		
+	if direction > 0:
+		animated_sprite.flip_h = false
+	elif direction < 0:
+		animated_sprite.flip_h = true
 
 	move_and_slide()
