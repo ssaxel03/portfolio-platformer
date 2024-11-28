a1b880211bede0ab14ea61dd964033b32391ce92
axel <ssaxel03@gmail.com>
2024-11-28 11:38:21 +0000

    feat: added shooting mechanic

---
+++ scripts/bullet.gd
@@ -0,0 +1,30 @@
+extends Node2D
+
+@onready var timer: Timer = $DeSpawn
+
+
+const SPEED = 70
+const PARALLAX = preload("res://scenes/parallax.tscn") # TODO
+
+@export var direction: int = 0
+@export var tile_map: TileMapLayer = null
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	timer.start()
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float) -> void:
+	
+	var map_position = tile_map.local_to_map(self.global_position)
+	
+	if tile_map.get_cell_source_id(map_position) != -1:
+		queue_free()
+		return
+	
+	self.move_local_x(SPEED * direction)
+
+func _on_timer_timeout() -> void:
+	queue_free()
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,10 +1,13 @@
 extends CharacterBody2D
 
-
+const Bullet = preload("../scenes/bullet.tscn")
 const SPEED = 1200.0
 const JUMP_VELOCITY = -3000.0
 
 @onready var animated_sprite = $AnimatedSprite2D
+@onready var timer: Timer = $FireRate
+
+var canShoot = true
 
 func _ready() -> void:
 	animated_sprite.play("idle")
@@ -27,11 +30,13 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		
+	
+	if Input.is_action_just_pressed("shoot") and canShoot:
+		_shoot(animated_sprite.flip_h)
+	
+	# ANIMATIONS
 	if is_on_floor():
-		if direction > 0:
-			animated_sprite.play("run")
-		elif direction < 0:
+		if direction != 0:
 			animated_sprite.play("run")
 		else:
 			animated_sprite.play("idle")
@@ -44,3 +49,15 @@ func _physics_process(delta: float) -> void:
 		animated_sprite.flip_h = true
 
 	move_and_slide()
+	
+func _shoot(flip_h) -> void:
+	var bullet = Bullet.instantiate()
+	get_tree().root.add_child(bullet)
+	bullet.tile_map = get_node("%platforms")
+	bullet.global_position = self.global_position
+	bullet.direction = -1 if flip_h else 1
+	canShoot = false
+	timer.start()
+
+func _on_timer_timeout() -> void:
+	canShoot = true
