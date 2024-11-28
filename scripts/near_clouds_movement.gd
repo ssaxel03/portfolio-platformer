780c918979ced58373a3c6918a91977ff5d2ff3c
axel <ssaxel03@gmail.com>
2024-11-28 09:27:41 +0000

    fix: fixed parallax effect

---
+++ scripts/near_clouds_movement.gd
@@ -0,0 +1,15 @@
+extends Parallax2D
+
+@onready var near_clouds: Parallax2D = $"."
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _physics_process(delta: float) -> void:
+	near_clouds.scroll_offset.x -= 50 * delta
